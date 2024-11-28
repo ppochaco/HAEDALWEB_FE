01618fdc91034618247a0c151b4f2153c30f894f
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-11-29 01:51:20 +0900

    Fix: next.config 롤백

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,6 @@ const nextConfig = {
   images: {
     domains: ['haedal-image-storage.s3.ap-northeast-2.amazonaws.com'],
   },
-  async rewrites() {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+}
 
-    if (!baseUrl) {
-      throw new Error("환경 변수를 설정해주세요.");
-    }
-
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${baseUrl}/:path*`,
-      },
-    ];
-  },
-};
-
-export default nextConfig;
+export default nextConfig
