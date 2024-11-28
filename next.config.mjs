245225bbfb36159f19c261b7db7f7ae5e4c0a086
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-11-29 01:45:54 +0900

    Fix: 프록시 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['haedal-image-storage.s3.ap-northeast-2.amazonaws.com'],
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
